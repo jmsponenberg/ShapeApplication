510d6247abb49b12d0616892e7c42437ed5f25b9
Jeff Sponenberg <jmsponenberg@users.noreply.github.com>
2021-06-09 17:31:03 -0400

    Added Point Class with public floating point x and y values as prompted

---
+++ Point.cpp
@@ -0,0 +1,7 @@
+#include "Point.h"
+
+Point::Point(float x, float y)
+{
+	m_x = x;
+	m_y = y;
+}
---
+++ Point.h
@@ -0,0 +1,11 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+	public:
+		Point(float x, float y);
+		float m_x;
+		float m_y;
+};
+
+#endif
